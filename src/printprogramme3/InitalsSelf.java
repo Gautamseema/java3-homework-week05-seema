e21870109b4f1b799d625d68a5d7121e2b1c6b5b
Gautamseema <seematest244@gmail.com>
2023-10-12 13:47:01 +0100

    java3-homework-week05

---
+++ src/printprogramme3/InitalsSelf.java
@@ -0,0 +1,18 @@
+package printprogramme3;
+
+public class InitalsSelf {
+    public static void main(String[] args) {
+
+        System.out.println("  ******            **********");
+        System.out.println("  **    **          **      **       ");
+        System.out.println("   **    **        **      **        ");
+        System.out.println("     **           **                 ");
+        System.out.println("      **          **       ******    ");
+        System.out.println("       **         **       **        ");
+        System.out.println(" **    **          **      **        ");
+        System.out.println(" **   **            **     **        ");
+        System.out.println(" *****              *********        ");
+
+
+    }
+}
